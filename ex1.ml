4a3d2a2b8c10166eca289de88fc2b9e1228cf8aa
Alexander Markov <apsheronets@gmail.com>
2013-05-05 12:24:43 +0400

    some asics analytics

--- ex1.ml
+++ ex1.ml
@@ -6,15 +6,11 @@
 module P = Gnuplot
 open Parse_args
 
-let course ?(reward=25.) hashrate difficulty power_cost =
+let course ?(reward=25.) difficulty hashrate power_cost =
   let time_in_seconds = difficulty *. (2. ** 32.) /. hashrate in
   ((time_in_seconds /. 3600.) *. power_cost) /. reward
 
-let course power_cost = course 400000000. 7672999.92016 (power_cost /. 30.)
-
-let difficulty = 10076292.88341872
-let network_hashrate = 73650000000000.
-let current_blocks_per_second = 153. /. 24. /. 60. /. 60.
+let course power_cost = course 7672999.92016 400000000. (power_cost /. 30.)
 
 (*let difficulty = 3000000.0*)
 (*let hashrate = 400000000.*)
@@ -22,42 +18,42 @@ let current_blocks_per_second = 153. /. 24. /. 60. /. 60.
 let revard = 25.
 let time_for_revard difficulty hashrate = difficulty *. (2. ** 32.) /. hashrate
 let to_usd x = x /. 30.
-let energy_for_revard hashrate power = (time_for_revard difficulty hashrate) *. power
-let energy_per_coin hashrate power = (energy_for_revard hashrate power) /. revard
+let energy_for_revard difficulty hashrate power = (time_for_revard difficulty hashrate) *. power
+let energy_per_coin difficulty hashrate power = (energy_for_revard difficulty hashrate power) /. revard
 
-let course hashrate power power_rate =
-  (energy_per_coin hashrate power) *. (to_usd (power_rate /. 3600000.))
+let course difficulty hashrate power power_rate =
+  (energy_per_coin difficulty hashrate power) *. (to_usd (power_rate /. 3600000.))
 
-let proofit_per_second hashrate power power_rate =
-  (25. *. course hashrate power power_rate) /.
+let proofit_per_second difficulty hashrate power power_rate =
+  (25. *. course difficulty hashrate power power_rate) /.
   (time_for_revard difficulty hashrate)
 
 let amortization_per_second hardware_cost =
   let years = 1. in
   hardware_cost /. (years *. 365. *. 24. *. 60. *. 60.)
 
-let amortizated_course hardware_cost hashrate power power_rate =
-  ((proofit_per_second hashrate power power_rate +. amortization_per_second hardware_cost) *. (time_for_revard difficulty hashrate)) /. 25.
+let amortizated_course hardware_cost difficulty hashrate power power_rate =
+  ((proofit_per_second difficulty hashrate power power_rate +. amortization_per_second hardware_cost) *. (time_for_revard difficulty hashrate)) /. 25.
 
 let market_rate = 100.
 
-let income hashrate power power_rate =
-  let course = course hashrate power power_rate in
+let income difficulty hashrate power power_rate =
+  let course = course difficulty hashrate power power_rate in
   let course = market_rate -. course in
   (25. *. course) /.
   (time_for_revard difficulty hashrate)
 
-let income_per_day hashrate power power_rate =
-  (income hashrate power power_rate) *. 60. *. 60. *. 24.
+let income_per_day difficulty hashrate power power_rate =
+  (income difficulty hashrate power power_rate) *. 60. *. 60. *. 24.
 
-let amortizated_income hardware_cost hashrate power power_rate =
-  let course = amortizated_course hardware_cost hashrate power power_rate in
+let amortizated_income hardware_cost difficulty hashrate power power_rate =
+  let course = amortizated_course hardware_cost difficulty hashrate power power_rate in
   let course = market_rate -. course in
   (25. *. course) /.
   (time_for_revard difficulty hashrate)
 
-let amortizated_income_per_day hardware_cost hashrate power power_rate =
-  (amortizated_income hardware_cost hashrate power power_rate) *. 60. *. 60. *. 24.
+let amortizated_income_per_day hardware_cost difficulty hashrate power power_rate =
+  (amortizated_income hardware_cost difficulty hashrate power power_rate) *. 60. *. 60. *. 24.
 
 (*let time_for_revard difficulty hashrate = difficulty *. (2. ** 32.) /. hashrate*)
 
@@ -80,7 +76,27 @@ let blue = 0x0000FF
   P.box g;
   P.xy_file g "/home/komar/mtgox-all-time"*)
 
+let difficulty = 10076292.88341872
+let network_hashrate = 73650000000000.
+let current_blocks_per_second = 153. /. 24. /. 60. /. 60.
+
 let () =
+  let g = P.init ?offline:(offline 1) ~xsize:800. ~ysize:600. (device 1) in
+  P.box g;
+  P.xlabel g "count of asics";
+  P.ylabel g "income per day, $";
+
+  let f x =
+    let network_hashrate = network_hashrate +. (50000000000. *. x) in
+    let difficulty = difficulty_for_hashrate network_hashrate in
+    income_per_day difficulty 50000000000. 0. 3. in
+
+  P.color g red;
+  P.fx g f 0. 100000.;
+
+  P.close g
+
+(*let () =
   let g = P.init (*?offline:(offline 1) ~xsize:500. ~ysize:300.*) (device 1) in
   P.box g;
   P.xlabel g "watt-hour rate, rur";
@@ -88,9 +104,17 @@ let () =
 
   (* BitForce SC 50 Gh/s *)
   P.color g green;
-  P.fx g (course 50000000000. 1.) 0. 5.;
+  P.fx g (course difficulty 50000000000. 100.) 0. 5.;
   P.color g red;
-  P.fx g (amortizated_course 2499. 50000000000. 1.) 0. 5.;
+  P.fx g (amortizated_course 2499. difficulty 50000000000. 100.) 0. 5.;
+
+  (* BitForce SC 50 Gh/s when there will 40 000 of them *)
+  (let network_hashrate = network_hashrate +. (50000000000. *. 40000.) in
+  let difficulty = difficulty_for_hashrate network_hashrate in
+  P.color g green;
+  P.fx g (course difficulty 50000000000. 100.) 0. 5.;
+  P.color g red;
+  P.fx g (amortizated_course 2499. difficulty 50000000000. 100.) 0. 5.);
 
   (* 7990 *)
   (*P.color g green;
@@ -173,7 +197,7 @@ let () =
   P.fx g (course 123000000. 55.0) 0. 5.;*)
 
   P.close g
-
+*)
 
 (*let () =
   let g = P.init ?offline:(offline 1) ~xsize:500. ~ysize:300. (device 1) in
