e3e7c43a08274e74ec43ff51aa41dddd93266c29
Alexander Markov <apsheronets@gmail.com>
2013-07-02 20:57:19 +0400

    lite and bit comparison

--- ex1.ml
+++ ex1.ml
@@ -35,25 +35,23 @@ let amortization_per_second hardware_cost =
 let amortizated_course hardware_cost difficulty hashrate power power_rate =
   ((proofit_per_second difficulty hashrate power power_rate +. amortization_per_second hardware_cost) *. (time_for_revard difficulty hashrate)) /. 25.
 
-let market_rate = 100.
-
-let income difficulty hashrate power power_rate =
+let income market_rate difficulty hashrate power power_rate =
   let course = course difficulty hashrate power power_rate in
   let course = market_rate -. course in
   (25. *. course) /.
   (time_for_revard difficulty hashrate)
 
-let income_per_day difficulty hashrate power power_rate =
-  (income difficulty hashrate power power_rate) *. 60. *. 60. *. 24.
+let income_per_day market_rate difficulty hashrate power power_rate =
+  (income market_rate difficulty hashrate power power_rate) *. 60. *. 60. *. 24.
 
-let amortizated_income hardware_cost difficulty hashrate power power_rate =
+let amortizated_income hardware_cost market_rate difficulty hashrate power power_rate =
   let course = amortizated_course hardware_cost difficulty hashrate power power_rate in
   let course = market_rate -. course in
   (25. *. course) /.
   (time_for_revard difficulty hashrate)
 
-let amortizated_income_per_day hardware_cost difficulty hashrate power power_rate =
-  (amortizated_income hardware_cost difficulty hashrate power power_rate) *. 60. *. 60. *. 24.
+let amortizated_income_per_day hardware_cost makret_rate difficulty hashrate power power_rate =
+  (amortizated_income hardware_cost makret_rate difficulty hashrate power power_rate) *. 60. *. 60. *. 24.
 
 (*let time_for_revard difficulty hashrate = difficulty *. (2. ** 32.) /. hashrate*)
 
@@ -76,9 +74,13 @@ let blue = 0x0000FF
   P.box g;
   P.xy_file g "/home/komar/mtgox-all-time"*)
 
-let difficulty = 11187257.46136079
-let network_hashrate = 80.42 *. 1000000000000.
+(*let difficulty = 11187257.46136079*) (* before asics *)
+let difficulty = 21335329.11398300
+let litecoin_difficulty = 841.46756083
+let network_hashrate = 160.01 *. 1000000000000.
 let current_blocks_per_second = 153. /. 24. /. 60. /. 60.
+let market_rate = 90.
+let litecoin_market_rate = 2.7
 
 let asics_income () =
   let g = P.init ?offline:(offline 1) ~xsize:800. ~ysize:600. (device 1) in
@@ -90,12 +92,12 @@ let asics_income () =
   let income_per_day x =
     let network_hashrate = network_hashrate +. (50000000000. *. x) in
     let difficulty = difficulty_for_hashrate network_hashrate in
-    income_per_day difficulty 50000000000. 0. 3. in
+    income_per_day market_rate difficulty 50000000000. 0. 3. in
 
   let amortizated_income_per_day x =
     let network_hashrate = network_hashrate +. (50000000000. *. x) in
     let difficulty = difficulty_for_hashrate network_hashrate in
-    amortizated_income_per_day 2499. difficulty 50000000000. 0. 3. in
+    amortizated_income_per_day 2499. market_rate difficulty 50000000000. 0. 3. in
 
   P.color g green;
   P.fx g income_per_day 10000. 100000.;
@@ -135,7 +137,7 @@ let litecoin_videocards () =
   P.xlabel g "watt-hour rate, rur";
   P.ylabel g "litecoin price, $";
 
-  let difficulty = 558.12178821 in
+  let difficulty = litecoin_difficulty in
   let course = course difficulty in
   let amortizated_course hw_c = amortizated_course hw_c difficulty in
 
@@ -243,10 +245,37 @@ let videocards () =
 
   P.close g
 
+let lite_and_bit_comparison () =
+  let g = P.init (*?offline:(offline 1) ~xsize:500. ~ysize:300.*) (device 1) in
+  P.box g;
+  P.box g ~x:[P.tics ~grid:true ()] ~y:[P.tics ~grid:true ()];
+  P.xlabel g "watt-hour rate, rur";
+  P.ylabel g "income per day, $";
+
+  let bitcoin_income_per_day hashrate power power_rate =
+    income_per_day market_rate difficulty hashrate power power_rate in
+  let litecoin_income_per_day hashrate power power_rate =
+    income_per_day litecoin_market_rate litecoin_difficulty hashrate power power_rate in
+
+  (* 7970 *)
+  P.color g green;
+  P.fx g (bitcoin_income_per_day 650000000. 250.) 0. 5.;
+  P.color g red;
+  P.fx g (litecoin_income_per_day 650000. 250.) 0. 5.;
+
+  (* 7950 *)
+  P.color g green;
+  P.fx g (bitcoin_income_per_day 500000000. 200.) 0. 5.;
+  P.color g red;
+  P.fx g (litecoin_income_per_day 600000. 250.) 0. 5.;
+
+  P.close g
+
 (*let () = asics_income ()*)
 (*let () = asics_course ()*)
 (*let () = videocards ()*)
-let () = litecoin_videocards ()
+let () = lite_and_bit_comparison ()
+(*let () = litecoin_videocards ()*)
 
 (*let () =
   let g = P.init ?offline:(offline 1) ~xsize:500. ~ysize:300. (device 1) in
