9a323fd9a4176e9b3c72749ee5bc17b55e4e6f48
Alexander Markov <apsheronets@gmail.com>
2013-07-06 06:25:11 +0400

    higher resulution for bitcoin blocks

--- get-bitcoin-blocks.bash
+++ get-bitcoin-blocks.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget http://blockexplorer.com/q/nethash -O data/bitcoin-blocks.csv
+# 36 is for 6 hours resolution
+wget http://blockexplorer.com/q/nethash/36 -O data/bitcoin-blocks.csv
 tail -n +16 data/bitcoin-blocks.csv > data/bitcoin-blocks-wo-header.csv
 rm -f data/bitcoin-blocks.dat
 ruby csv-unixtime-to-dat.rb data/bitcoin-blocks-wo-header.csv data/bitcoin-blocks.dat
