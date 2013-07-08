e682e9048a4954f4d43f4cfc0670706f3eee96a6
Alexander Markov <apsheronets@gmail.com>
2013-07-08 23:49:25 +0400

    forgot to set $today

--- get-mtgox-btc-usd.bash
+++ get-mtgox-btc-usd.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget 'http://www.quandl.com/api/v1/datasets/BITCOIN/MTGOXUSD.csv?&trim_start=2010-07-17&trim_end=2013-07-04&collapse=daily&sort_order=desc' -O data/mtgox-btc-usd.csv
+today=`date +%Y-%m-%d`
+wget "http://www.quandl.com/api/v1/datasets/BITCOIN/MTGOXUSD.csv?&trim_start=2010-07-17&trim_end=${today}&collapse=daily&sort_order=desc" -O data/mtgox-btc-usd.csv
 tail -n +2 data/mtgox-btc-usd.csv > data/mtgox-btc-usd-wo-header.csv
 sed -i 's/-//g'  data/mtgox-btc-usd-wo-header.csv
 sed -i 's/,/ /g' data/mtgox-btc-usd-wo-header.csv
