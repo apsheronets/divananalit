79879463b08506b6c45a01be5dc8f0f8b4851eed
Alexander Markov <apsheronets@gmail.com>
2013-07-09 06:59:17 +0400

    note for btc-e-ltc-usd

--- litecoin-self-cost.bash
+++ litecoin-self-cost.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 make litecoin_self_cost
 ./get-litecoin-blocks.bash
+# please get litecoin rates from http://www.cryptocoincharts.info/period-charts.php?period=alltime&resolution=day&pair=ltc-usd&market=btc-e by yourself
 ./litecoin_self_cost < data/litecoin-blocks.dat > data/litecoin-self-cost.dat
 ./litecoin-self-cost.gnuplot
