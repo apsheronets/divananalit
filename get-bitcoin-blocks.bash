155ab1ef0254e70d89d95d53302f66aab12f4c61
Alexander Markov <apsheronets@gmail.com>
2013-11-20 20:01:36 +0400

    basedir for get- scripts

--- get-bitcoin-blocks.bash
+++ get-bitcoin-blocks.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+basedir=`dirname $0`
+
 # 36 is for 6 hours resolution
 wget http://blockexplorer.com/q/nethash/18 -O data/bitcoin-blocks.csv || exit 1
 tail -n +16 data/bitcoin-blocks.csv > data/bitcoin-blocks-wo-header.csv
 rm -f data/bitcoin-blocks.dat
-ruby csv-unixtime-to-dat.rb data/bitcoin-blocks-wo-header.csv data/bitcoin-blocks.dat
+ruby $basedir/csv-unixtime-to-dat.rb data/bitcoin-blocks-wo-header.csv data/bitcoin-blocks.dat
 rm -f data/bitcoin-blocks-wo-header.csv
--- get-litecoin-blocks.bash
+++ get-litecoin-blocks.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+basedir=`dirname $0`
+
 wget http://explorer.litecoin.net/chain/Litecoin/q/nethash -O data/litecoin-blocks || exit 1
 tail -n +18 data/litecoin-blocks > data/litecoin-blocks-wo-header.csv || exit 1
 rm -f data/litecoin-blocks.dat
-ruby csv-unixtime-to-dat.rb data/litecoin-blocks-wo-header.csv data/litecoin-blocks.dat || exit 1
+ruby $basedir/csv-unixtime-to-dat.rb data/litecoin-blocks-wo-header.csv data/litecoin-blocks.dat || exit 1
 rm -f data/litecoin-blocks-wo-header.csv
