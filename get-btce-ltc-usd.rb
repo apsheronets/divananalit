bc8fe60f44adfd157050a156087133b951513d21
Alexander Markov <apsheronets@gmail.com>
2013-11-27 13:16:31 +0400

    use pipes instead of ruby's File crap

--- get-btce-ltc-usd.rb
+++ get-btce-ltc-usd.rb
@@ -44,17 +44,13 @@ url = 'http://www.cryptocoincharts.info/period-charts.php?period=alltime&resolut
 page = get agent, url
 
 trs = page.root.search('div[@class="container"]/div[@class="row"]/div[@class="span10"]/table[@class="table table-striped"]/tbody/tr')
-File.open(ARGV[0], 'w') do |file|
-  trs.each do |tr|
-    tds = tr.search("td")
-    date = tds[0].inner_text.strip.gsub(/-/, '')
-    low   = tds[1].inner_text.gsub(/USD/, '').strip
-    high  = tds[2].inner_text.gsub(/USD/, '').strip
-    open  = tds[3].inner_text.gsub(/USD/, '').strip
-    close = tds[4].inner_text.gsub(/USD/, '').strip
-    volume = tds[5].inner_text.gsub(/LTC/, '').gsub(/,/, '').strip
-    file.write("#{date} #{low} #{high} #{open} #{close} #{volume}\n")
-  end
-  file.close
+trs.each do |tr|
+  tds = tr.search("td")
+  date = tds[0].inner_text.strip.gsub(/-/, '')
+  low   = tds[1].inner_text.gsub(/USD/, '').strip
+  high  = tds[2].inner_text.gsub(/USD/, '').strip
+  open  = tds[3].inner_text.gsub(/USD/, '').strip
+  close = tds[4].inner_text.gsub(/USD/, '').strip
+  volume = tds[5].inner_text.gsub(/LTC/, '').gsub(/,/, '').strip
+  print "#{date} #{low} #{high} #{open} #{close} #{volume}\n"
 end
-
