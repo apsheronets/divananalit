e1ffa92576656b50b20459e80e26bd50ad233e8e
Alexander Markov <apsheronets@gmail.com>
2014-01-30 11:49:16 +0400

    cleanup in scrapers

--- get-btce-ltc-usd.rb
+++ get-btce-ltc-usd.rb
@@ -1,38 +1,4 @@
 require 'mechanize'
-require 'digest/sha1'
-
-def smart_www_action(&blk)
-  attempts = 10
-  begin
-    blk.call
-  rescue Mechanize::ResponseCodeError => e
-    if e.response_code == "404"
-      raise e
-    else
-      attempts -= 1
-      if attempts <= 0
-        raise e
-      else
-        retry
-      end
-    end
-  rescue => e
-    attempts -= 1
-    if attempts <= 0
-      raise e
-    else
-      retry
-    end
-  end
-end
-
-def get(www, *args)
-  # debug
-  #puts "GET in #{caller[0]}: #{args.inspect}"
-  smart_www_action do
-    www.get *args
-  end
-end
 
 agent = Mechanize.new { |agent|
           agent.history.max_size=1
@@ -41,7 +7,7 @@ agent = Mechanize.new { |agent|
 
 url = 'http://www.cryptocoincharts.info/period-charts.php?period=alltime&resolution=day&pair=ltc-usd&market=btc-e'
 
-page = get agent, url
+page = agent.get url
 
 trs = page.root.search('div[@class="container"]/div[@class="row"]/div[@class="span10"]/table[@class="table table-striped"]/tbody/tr')
 trs.each do |tr|
