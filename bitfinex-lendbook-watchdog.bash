9a84330ace040f63b62b74862ad9ac1b0967b028
Alexander Markov <apsheronets@gmail.com>
2014-06-15 00:59:44 +0400

    missing "done"

--- bitfinex-lendbook-watchdog.bash
+++ bitfinex-lendbook-watchdog.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true; do for sym in usd btc ltc; do curl -s https://api.bitfinex.com/v1/lendbook/$sym | ruby ./bitfinex-lendbook-parser.rb data/bitfinex-lendbook-${sym}.dat; sleep 10; done;
+while true; do for sym in usd btc ltc; do curl -s https://api.bitfinex.com/v1/lendbook/$sym | ruby ./bitfinex-lendbook-parser.rb data/bitfinex-lendbook-${sym}.dat; sleep 10; done; done;
