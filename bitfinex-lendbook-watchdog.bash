beae9cc26b3aa2bfc6d3593ee33f11752c22f340
Alexander Markov <apsheronets@gmail.com>
2014-06-15 01:05:05 +0400

    extra kludges to run this shit

--- bitfinex-lendbook-watchdog.bash
+++ bitfinex-lendbook-watchdog.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
-while true; do for sym in usd btc ltc; do curl -s https://api.bitfinex.com/v1/lendbook/$sym | ruby ./bitfinex-lendbook-parser.rb data/bitfinex-lendbook-${sym}.dat; sleep 10; done; done;
+basedir=`dirname $0`
+while true; do for sym in usd btc ltc; do curl -s https://api.bitfinex.com/v1/lendbook/$sym | ruby $basedir/bitfinex-lendbook-parser.rb data/bitfinex-lendbook-${sym}.dat; sleep 10; done; done;
