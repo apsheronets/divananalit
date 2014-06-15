8f25a8fa04194d76ee61e4ca89022c825df30d8b
Alexander Markov <apsheronets@gmail.com>
2014-06-15 05:05:59 +0400

    bitfinex watchdog: sleep 5

--- bitfinex-lendbook-watchdog.bash
+++ bitfinex-lendbook-watchdog.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 basedir=`dirname $0`
-while true; do for sym in usd btc ltc; do curl -s https://api.bitfinex.com/v1/lendbook/$sym | ruby $basedir/bitfinex-lendbook-parser.rb data/bitfinex-lendbook-${sym}.dat; sleep 10; done; done;
+while true; do for sym in usd btc ltc; do curl -s https://api.bitfinex.com/v1/lendbook/$sym | ruby $basedir/bitfinex-lendbook-parser.rb data/bitfinex-lendbook-${sym}.dat; sleep 5; done; done;
