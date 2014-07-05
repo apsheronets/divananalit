3b374f120b7eafda9105f855cfaf607926fa9ad8
Alexander Markov <apsheronets@gmail.com>
2014-07-05 10:50:44 +0400

    using fucking selenium

--- get-bitfinex-sentiment-index.rb
+++ get-bitfinex-sentiment-index.rb
@@ -1,13 +1,28 @@
 require 'mechanize'
 
-agent = Mechanize.new { |agent|
-          agent.history.max_size=1
-          agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        }
+#agent = Mechanize.new { |agent|
+#          agent.history.max_size=1
+#          agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#        }
+#
+#url = 'https://www.bitfinex.com/pages/stats'
+#
+#page = agent.get url
 
-url = 'https://www.bitfinex.com/pages/stats'
+# Fucking bitfinex added Web3.99 shit-tastic JS-based SaaS DoS-protection
+# using fucking selenium
+# it's 2014 and I couldn't just download fucking HTML
+# holy fuck
 
-page = agent.get url
+require 'rubygems'
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for :firefox
+driver.get "https://www.bitfinex.com/pages/stats"
+
+page = Nokogiri::HTML(driver.page_source)
+
+driver.quit
 
 green = page.root.search('div[@class="progress large-12 success radius"]/span').first.attributes['style'].value.match(/width:\s*([0-9.]+)%/)[1].to_f
 red = page.root.search('div[@class="progress large-12 red secondary radius"]/span').first.attributes['style'].value.match(/width:\s*([0-9.]+)%/)[1].to_f
