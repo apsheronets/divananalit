7c25441b75eea406e2e34ba0875615a31a2b0849
Alexander Markov <apsheronets@gmail.com>
2014-08-29 18:43:33 +0400

    lexszero's server fucked up

--- get-litecoin-blocks.bash
+++ get-litecoin-blocks.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 basedir=`dirname $0`
-wget -q -c http://komar.lexs.blasux.ru/litecoin-blocks-difficulty -O $basedir/data/litecoin-blocks
+wget -q -c http://komar.lexs.blasux.ru:8888/litecoin-blocks-difficulty -O $basedir/data/litecoin-blocks
