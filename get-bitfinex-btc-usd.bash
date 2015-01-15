8708a5e62d20d864fda5050980e3f528f968f207
Alexander Markov <apsheronets@gmail.com>
2015-01-15 18:42:56 +0300

    curl -s

--- get-bitfinex-btc-usd.bash
+++ get-bitfinex-btc-usd.bash
@@ -6,5 +6,5 @@ basedir=`dirname $0`
 filename=bitfinex-btc-usd.dat
 tmpfile=/tmp/${filename}-`date +%s`
 
-curl 'http://bitcoincharts.com/charts/chart.json?m=bitfinexUSD&SubmitButton=Draw&r=&i=&c=0&s=&e=&Prev=&Next=&t=S&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=1&ps=0&l=0&p=0&' | ruby $basedir/parse-bitcoincharts-raw-data.rb > $tmpfile
+curl -s 'http://bitcoincharts.com/charts/chart.json?m=bitfinexUSD&SubmitButton=Draw&r=&i=&c=0&s=&e=&Prev=&Next=&t=S&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=1&ps=0&l=0&p=0&' | ruby $basedir/parse-bitcoincharts-raw-data.rb > $tmpfile
 mv $tmpfile $basedir/data/${filename}
