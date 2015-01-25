9fa86d784b05458ccfcaaeeca4eb475daae5f29d
Alexander Markov <apsheronets@gmail.com>
2015-01-25 12:17:47 +0300

    whoops

--- update-bitfinex-orderbook.bash
+++ update-bitfinex-orderbook.bash
@@ -4,4 +4,4 @@ source $HOME/.rvm/scripts/rvm
 basedir=`dirname $0`
 
 cd $basedir
-curl -s 'https://api.bitfinex.com/v1/book/btcusd' | bundle exec ruby ./bitfinex-orderbook.rb"
+curl -s 'https://api.bitfinex.com/v1/book/btcusd' | bundle exec ruby ./bitfinex-orderbook.rb
