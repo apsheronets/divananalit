c16387e509d3fe421a97b18b6705841f08a14250
Alexander Markov <apsheronets@gmail.com>
2015-02-01 08:28:32 +0300

    lends.orderbook_id removed

--- bitfinex-lends.rb
+++ bitfinex-lends.rb
@@ -9,11 +9,11 @@ require 'httparty'
 c = PG.connect( dbname: 'divananalit' )
 c.set_error_verbosity( PG::PQERRORS_VERBOSE )
 
-c.prepare( 'last_timestamp', "SELECT timestamp FROM lends WHERE orderbook_id = (SELECT id FROM orderbooks WHERE exchange = 'bitfinex' AND pair = 'btcusd' LIMIT 1) ORDER BY timestamp DESC LIMIT 1;" )
+c.prepare( 'last_timestamp', "SELECT timestamp FROM lends WHERE currency = 'btc' AND exchange = 'bitfinex' ORDER BY timestamp DESC LIMIT 1;" )
 
-c.prepare( 'check_if_lend_exist', "SELECT 1 FROM lends WHERE orderbook_id = (SELECT id FROM orderbooks WHERE exchange = 'bitfinex' AND pair = 'btcusd' LIMIT 1) AND timestamp = to_timestamp($1) AND rate = $2 AND amount = $3 LIMIT 1;" )
+c.prepare( 'check_if_lend_exist', "SELECT 1 FROM lends WHERE currency = 'btc' AND exchange = 'bitfinex' AND timestamp = to_timestamp($1) AND rate = $2 AND amount = $3 LIMIT 1;" )
 
-c.prepare( 'insert_lend', "INSERT INTO lends (orderbook_id, rate, amount, timestamp) VALUES ((SELECT id FROM orderbooks WHERE exchange = 'bitfinex' AND pair = 'btcusd' LIMIT 1), $1, $2, to_timestamp($3));" )
+c.prepare( 'insert_lend', "INSERT INTO lends (currency, exchange, rate, amount, timestamp) VALUES ('btc', 'bitfinex', $1, $2, to_timestamp($3));" )
 
 while true do
 
