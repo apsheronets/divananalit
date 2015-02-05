153d2b06916c097c39aba4beab5ddebbd058cbc2
Alexander Markov <apsheronets@gmail.com>
2015-02-05 22:27:26 +0300

    all currencies

--- bitfinex-lends.rb
+++ bitfinex-lends.rb
@@ -11,35 +11,36 @@ c.set_error_verbosity( PG::PQERRORS_VERBOSE )
 
 c.exec( 'SET TIME ZONE UTC' )
 
-c.prepare( 'last_timestamp', "SELECT extract (epoch from (timestamp)) AS timestamp FROM lends WHERE currency = 'btc' AND exchange = 'bitfinex' ORDER BY timestamp DESC LIMIT 1;" )
+c.prepare( 'last_timestamp', "SELECT extract (epoch from (timestamp)) AS timestamp FROM lends WHERE currency = $1 AND exchange = 'bitfinex' ORDER BY timestamp DESC LIMIT 1;" )
 
-c.prepare( 'check_if_lend_exist', "SELECT 1 FROM lends WHERE currency = 'btc' AND exchange = 'bitfinex' AND timestamp = to_timestamp($1) AND rate = $2 AND amount = $3 LIMIT 1;" )
+c.prepare( 'check_if_lend_exist', "SELECT 1 FROM lends WHERE currency = $1 AND exchange = 'bitfinex' AND timestamp = to_timestamp($2) AND rate = $3 AND amount = $4 LIMIT 1;" )
 
-c.prepare( 'insert_lend', "INSERT INTO lends (currency, exchange, rate, amount, timestamp) VALUES ('btc', 'bitfinex', $1, $2, to_timestamp($3));" )
+c.prepare( 'insert_lend', "INSERT INTO lends (currency, exchange, rate, amount, timestamp) VALUES ($1, 'bitfinex', $2, $3, to_timestamp($4));" )
 
 while true do
+  ['btc', 'usd', 'ltc'].each do |currency|
 
-  timestamp = nil
-  c.exec_prepared('last_timestamp') do |result|
-    unless result.first.nil?
-      timestamp = result.first['timestamp']
-    end
-  end
-
-  response = HTTParty.get("https://api.bitfinex.com/v1/lends/btc", :query => {:timestamp => timestamp, :limit_lends => 9999})
-  json = JSON.parse(response.body)
-
-  json.each do |lend|
-    timestamp = lend['timestamp'].to_f
-    rate = lend['rate'].to_f
-    amount = lend['amount_lent'].to_f
-    c.exec_prepared( 'check_if_lend_exist', [timestamp, rate, amount] ) do |result|
-      if result.first.nil?
-        c.exec_prepared( 'insert_lend', [rate, amount, timestamp] )
+    timestamp = nil
+    c.exec_prepared('last_timestamp', [currency]) do |result|
+      unless result.first.nil?
+        timestamp = result.first['timestamp']
       end
     end
+
+    response = HTTParty.get("https://api.bitfinex.com/v1/lends/#{currency}", :query => {:timestamp => timestamp, :limit_lends => 9999})
+    json = JSON.parse(response.body)
+
+    json.each do |lend|
+      timestamp = lend['timestamp'].to_f
+      rate = lend['rate'].to_f
+      amount = lend['amount_lent'].to_f
+      c.exec_prepared( 'check_if_lend_exist', [currency, timestamp, rate, amount] ) do |result|
+        if result.first.nil?
+          c.exec_prepared( 'insert_lend', [currency, rate, amount, timestamp] )
+        end
+      end
+    end
+
+    sleep 10
   end
-
-  sleep 10
-
 end
