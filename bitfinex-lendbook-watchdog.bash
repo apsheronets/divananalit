8bbcf73951c885d9e33ddc1a58d7aac3599d3643
Alexander Markov <apsheronets@gmail.com>
2016-05-28 20:18:48 +0300

    th1 replaced with eth

--- bitfinex-lendbook-watchdog.bash
+++ bitfinex-lendbook-watchdog.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 source $HOME/.rvm/scripts/rvm
 basedir=`dirname $0`
-while true; do for sym in usd btc ltc th1; do curl -s https://api.bitfinex.com/v1/lendbook/$sym | ruby $basedir/bitfinex-lendbook-parser.rb data/bitfinex-lendbook-${sym}.dat; sleep 2; done; done;
+while true; do for sym in usd btc ltc eth; do curl -s https://api.bitfinex.com/v1/lendbook/$sym | ruby $basedir/bitfinex-lendbook-parser.rb data/bitfinex-lendbook-${sym}.dat; sleep 2; done; done;
