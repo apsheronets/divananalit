01616a16ad50a4f7b19e0c2e4b0fb9cd35025da2
Alexander Markov <apsheronets@gmail.com>
2016-08-06 23:34:21 +0300

    bitfinex fucked up(((

--- bitfinex-trades.bash
+++ bitfinex-trades.bash
@@ -4,4 +4,4 @@ source $HOME/.rvm/scripts/rvm
 basedir=`dirname $0`
 
 cd $basedir
-while true; do bundle exec ruby bitfinex-trades.rb; done
+while true; do bundle exec ruby bitfinex-trades.rb; sleep 60; done
